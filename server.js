c8a02b3e354dabf2b56262a5779d353e1b9b3dd8
agenosa <agenosa@my.bcit.ca>
2022-01-11 15:44:55 -0800

    added cars db2

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json())
+
+const cars = [
+  {
+    id: 1,
+    make: 'acura',
+    model: "nsx",
+    color: 'blue',
+  },
+  {
+    id: 2,
+    make: 'honda',
+    model: "civic",
+    color: 'green',
+  }
+]
+
+app.get("/api/cars", (req, res) => {
+  res.send(cars)
+});
+
+app.post("/api/cars", (req, res) => {
+  const data = req.body
+  data.id = cars.length+1
+  cars.push(data)
+  res.send(data)
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`listening on port ${port}`))
